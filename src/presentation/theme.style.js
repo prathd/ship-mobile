00c781a448326076a0b0bc390545c1f410103843
Prathmesh Desai <d.prathmesh@gmail.com>
2018-09-30 18:11:10 -0400

    add a global theme file

---
+++ src/presentation/theme.style.js
@@ -0,0 +1,27 @@
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+
+export default {
+  // colors
+  WHITE: '#FFFFFF',
+  PINK: '#F55D84',
+  PURPLE: '#7659DD',
+  LIGHT_PURPLE: '#8F5ACB',
+  FADED_PURPLE: '#CAADF2',
+
+  // font
+  FF_BLACK: 'Avenir-Black',
+  FF_BOOK: 'Avenir-Book',
+  FF_HEAVY: 'Avenir-Heavy',
+  FF_LIGHT: 'Avenir-Light',
+  FF_MEDIUM: 'Avenir-Medium',
+
+  FS_XS: hp('1.5%'),
+  FS_SM: hp('2%'),
+  FS_MD: hp('3%'),
+  FS_LG: hp('4%'),
+  FS_XL: hp('5%'),
+  FS_XXL: hp('6%'),
+};
