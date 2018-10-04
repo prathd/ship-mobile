cba357785ba7ba889917732e06fdbfb997e72845
Prathmesh Desai <d.prathmesh@gmail.com>
2018-10-04 14:38:08 -0400

    create rest of signup flow pages (incomplete)

--- src/presentation/containers/CreateAccountScreen.js
+++ src/presentation/containers/CreateAccountScreen.js
@@ -3,30 +3,20 @@ import { connect } from 'react-redux';
 import { TouchableOpacity } from 'react-native';
 import { bindActionCreators } from 'redux';
 
-import theme from '../theme.style';
 import { SCREENS } from '../navigation/screens';
 
 import SignupInput from '../components/blocks/SignupInput';
 import VerifyCode from '../components/blocks/VerifyCode';
-import Text from '../components/elements/Text';
 import CircleNextButton from '../components/elements/CircleNextButton';
 
 type Props = {};
-export class PhoneConfirm extends Component<Props> {
+export class CreateAccount extends Component<Props> {
   render() {
     return (
-      <SignupInput
-        back={this.popScreen}
-        prompt={`Enter the 6 digit code${'\n'}sent to your device`}
-      >
+      <SignupInput back={this.popScreen} prompt={`Account Details`}>
         <VerifyCode />
         <TouchableOpacity onPress={this.pushNextScreen}>
           <CircleNextButton />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={this.resendCode}>
-          <Text
-            modifiers={['heavy', 'xs', 'purple', 'vMargin10']}
-          >{`Didn't get a text?`}</Text>
         </TouchableOpacity>
       </SignupInput>
     );
@@ -39,7 +29,7 @@ export class PhoneConfirm extends Component<Props> {
   pushNextScreen = async () => {
     await this.props.push({
       component: {
-        name: SCREENS.ENTER_NAME,
+        name: SCREENS.DASHBOARD,
         options: {
           topBar: {
             visible: false,
@@ -68,7 +58,7 @@ const mapDispatchToProps = dispatch => {
   return bindActionCreators({}, dispatch);
 };
 
-export const PhoneConfirmScreen = connect(
+export const CreateAccountScreen = connect(
   mapStateToProps,
   mapDispatchToProps,
-)(PhoneConfirm);
+)(CreateAccount);
--- src/presentation/navigation/screens.js
+++ src/presentation/navigation/screens.js
@@ -13,11 +13,17 @@ import Compositor from './Compositor';
 import { LoginScreen } from '../containers/LoginScreen';
 import { PhoneScreen } from '../containers/PhoneScreen';
 import { PhoneConfirmScreen } from '../containers/PhoneConfirmScreen';
+import { EnterNameScreen } from '../containers/EnterNameScreen';
+import { EnterBirthdayScreen } from '../containers/EnterBirthdayScreen';
+import { CreateAccountScreen } from '../containers/CreateAccountScreen';
 
 export const SCREENS = {
   LOGIN: `navigation.app.LoginScreen`,
   PHONE: `navigation.app.PhoneScreen`,
   PHONE_CONFIRM: `navigation.app.PhoneConfirmScreen`,
+  ENTER_NAME: `navigation.app.EnterNameScreen`,
+  ENTER_BIRTHDAY: `navigation.app.EnterBirthdayScreen`,
+  CREATE_ACCOUNT: `navigation.app.CreateAccountScreen`,
 };
 
 export const registerScreens = () => {
@@ -36,5 +42,20 @@ export const registerScreens = () => {
   Navigation.registerComponent(
     SCREENS.PHONE_CONFIRM,
     decorateWithProvider(PhoneConfirmScreen, store, Provider),
+  );
+
+  Navigation.registerComponent(
+    SCREENS.ENTER_NAME,
+    decorateWithProvider(EnterNameScreen, store, Provider),
+  );
+
+  Navigation.registerComponent(
+    SCREENS.ENTER_BIRTHDAY,
+    decorateWithProvider(EnterBirthdayScreen, store, Provider),
+  );
+
+  Navigation.registerComponent(
+    SCREENS.CREATE_ACCOUNT,
+    decorateWithProvider(CreateAccountScreen, store, Provider),
   );
 };
