2beb69d9bb6df997132918c4b002bc2393e8ee29
Prathmesh Desai <d.prathmesh@gmail.com>
2018-10-09 20:53:31 -0400

    add margin for Create Account button

--- src/presentation/components/blocks/CreateAccountButton/RelativeView.js
+++ src/presentation/components/blocks/CreateAccountButton/RelativeView.js
@@ -2,9 +2,11 @@ import styled from 'styled-components';
 
 import View from '../../elements/View';
 
-import { moderateScale } from '../../../scale.utils';
+import { moderateScale, verticalScale } from '../../../scale.utils';
 
 export default styled(View)`
   width: ${moderateScale(292)};
   height: ${moderateScale(50)};
+
+  margin-top: ${verticalScale(15)};
 `;
